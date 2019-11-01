afe4355cf149090e87b54665cfed5fdc2229d8f7
Manh Duc Nguyen <36095261+manh17@users.noreply.github.com>
2019-11-01 23:17:34 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     authentication: 'plain',
-    user_name: 'manh11117@gmail.com',
-    password:  'Abc@0123456789',
+    user_name: '*********@gmail.com',
+    password:  '**************',
     domain:    'heroku.com',
     enable_starttls_auto: true
   }
