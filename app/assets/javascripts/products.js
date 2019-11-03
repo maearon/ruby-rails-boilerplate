f9f5e5247d81c33c68662b5f9351b8f75aacf9a9
manh <manh11117@gmail.com>
2019-11-03 10:10:52 +0700

    new layout

--- app/assets/javascripts/products.js
+++
@@ -1,8 +0,0 @@
-function myFunction(imgs) {
-  var expandImg = document.getElementById("expandedImg");
-  expandImg.src = imgs.src;
-  document.querySelectorAll('*[id^="thumbnail_"]').forEach(function(image){
-    image.setAttribute('style', "");
-  });
-  imgs.setAttribute("style", "border: 2px solid #000");
-}
