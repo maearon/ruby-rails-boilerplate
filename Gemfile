e95c5da1b63ee552799f9a6c09047ed5b1c65b08
manh <manhnd@2nf.com.vn>
2020-01-03 17:24:54 +0700

    config

--- Gemfile
+++ Gemfile
@@ -46,7 +46,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'pg'
+gem 'mysql2'
 gem 'kaminari', '~> 1.1', '>= 1.1.1'
 gem "ransack", "~> 2.3"
 gem "react-rails", "~> 2.6"
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_09_30_020813) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -25,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -36,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "cart_items", force: :cascade do |t|
+  create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "cart_id", null: false
     t.bigint "product_id", null: false
@@ -48,14 +45,14 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["variant_id"], name: "index_cart_items_on_variant_id"
   end
 
-  create_table "carts", force: :cascade do |t|
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
-  create_table "guest_cart_items", force: :cascade do |t|
+  create_table "guest_cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "guest_cart_id", null: false
     t.bigint "product_id", null: false
@@ -67,12 +64,12 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["variant_id"], name: "index_guest_cart_items_on_variant_id"
   end
 
-  create_table "guest_carts", force: :cascade do |t|
+  create_table "guest_carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "guest_wish_items", force: :cascade do |t|
+  create_table "guest_wish_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "guest_wish_id", null: false
     t.bigint "product_id", null: false
     t.bigint "variant_id", null: false
@@ -83,12 +80,12 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["variant_id"], name: "index_guest_wish_items_on_variant_id"
   end
 
-  create_table "guest_wishes", force: :cascade do |t|
+  create_table "guest_wishes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "microposts", force: :cascade do |t|
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -97,7 +94,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table "order_items", force: :cascade do |t|
+  create_table "order_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "order_id", null: false
     t.bigint "product_id", null: false
@@ -109,14 +106,14 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["variant_id"], name: "index_order_items_on_variant_id"
   end
 
-  create_table "orders", force: :cascade do |t|
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "jan_code"
     t.string "gender"
@@ -133,14 +130,14 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "projects", force: :cascade do |t|
+  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "relationships", force: :cascade do |t|
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
@@ -150,7 +147,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "reviewer"
     t.text "content"
     t.bigint "product_id", null: false
@@ -159,7 +156,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "description"
     t.boolean "done"
     t.bigint "project_id"
@@ -168,7 +165,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
@@ -184,7 +181,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "variants", force: :cascade do |t|
+  create_table "variants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "color"
     t.float "price"
     t.float "originalprice"
@@ -196,7 +193,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["product_id"], name: "index_variants_on_product_id"
   end
 
-  create_table "wish_items", force: :cascade do |t|
+  create_table "wish_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "wish_id", null: false
     t.bigint "product_id", null: false
     t.bigint "variant_id", null: false
@@ -207,7 +204,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_020813) do
     t.index ["wish_id"], name: "index_wish_items_on_wish_id"
   end
 
-  create_table "wishes", force: :cascade do |t|
+  create_table "wishes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
