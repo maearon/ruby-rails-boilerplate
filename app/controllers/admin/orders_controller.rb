962f0e64b1550cb9d32e8ffde86179fc90bcba6b
manh <manhnd@2nf.com.vn>
2020-01-17 15:54:01 +0700

    upgrade_admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,53 +1,113 @@
 class Admin::OrdersController < ApplicationController
   layout "admin"
-      before_action :find_user, only: %i[edit update destroy]
+  def index
+    @orders = Order.order(:id).page(params[:page]).per(10)
+  end
 
-    def index
-      @orders = Order.all.order(:id).page(params[:page]).per(10)
-    end
+  def show
+    @order = Order.find(params[:id])
+  end
 
-    def new
-      @user = User.new
-    end
+  def new
+    @order = Order.new
+  end
 
-    def edit; end
+  def edit
+    @order = Order.find(params[:id])
+  end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        redirect_to admin_users_path
+  def checkout1
+  end
+
+  def checkout2
+  end
+
+  def checkout3
+  end
+
+  def checkout4
+    @order = Order.new
+  end
+
+  def checkout5
+  end
+
+  def addresses
+  end
+
+  def create
+    # Generate order_items for a subset of vatiants (products) in current_cart (current_order).
+
+    unless current_cart.list.blank? || current_user.blank?
+      order = current_user.orders.create
+
+      current_cart.list.each { |cart_item|
+        order.order_items.create!(quantity: cart_item.quantity,
+                                  order_id: order.id,product_id: cart_item.product_id ,variant_id: cart_item.variant_id)
+      }
+
+      if order.save
+        redirect_to checkout5_path
       else
-        render :new
+        redirect_to request.referrer
       end
     end
 
-    def update
-      if @user.update(user_params)
-        redirect_to admin_users_path
-      else
-        render :edit
-      end
-    end
+    # @product = Product.find(params[:product_id])
+    # @variant = Variant.find(params[:variant_id])
+    # current_cart.cart(@product, @variant, params[:cart_item][:quantity], params[:cart_item][:action])
+    # respond_to do |format|
+    #   format.html { redirect_to request.referrer }
+    #   format.js
+    # end
 
-    def destroy
-      @user.destroy
-      respond_to do |format|
-        format.js
-      end
+
+    # def cart(product, variant, quantity, action)
+    #   if current_item = cart_items.find_by(variant: variant)
+    #     current_item.quantity ||= 1
+    #     action == 'edit' ? current_item.quantity = quantity.to_i : current_item.quantity += quantity.to_i
+    #     current_item.save
+    #   else
+    #     cart_items.create(product: product,variant: variant, cart: self, quantity: quantity)
+    #   end
+    # end
+
+
+
+    # @order = Order.new(order_params)
+
+    # if @order.save
+    #   redirect_to orders_path
+    # else
+    #   redirect_to request.referrer
+    # end
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to admin_orders_path
+    else
+      render 'edit'
     end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+
+    redirect_to request.referrer
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
-
-    def find_user
-      @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
-    end
+  def order_params
+    params.require(:order).permit(:user_id, :address, :phone, :method, :status, :details)
+  end
+  def cart_params
+    params.require(:cart).permit(:quantity, :cart_id, :product_id, :product_id, :variant_id)
+  end
+  def filtering_params(params)
+    params.slice(:status, :location, :starts_with)
+  end
 end
