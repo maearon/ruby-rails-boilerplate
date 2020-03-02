83df3a897207678f034cd67aa2199c552ebe7604
manhnd3 <manhnd@2nf.com.vn>
2020-03-02 10:25:22 +0700

    fix reset email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = 'protected-basin-52129.herokuapp.com'
+  host = 'clotheshub.herokuapp.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https'}
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
