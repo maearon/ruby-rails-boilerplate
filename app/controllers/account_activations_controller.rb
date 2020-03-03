161ae6f48d96fa6a230bd2a120683befec9572f5
mdnv <mdnvng@gmail.com>
2020-03-03 10:25:19 -0500

    config

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,6 +5,7 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
+      remember(user)
       flash[:success] = "Account activated!"
       redirect_to user
     else
