9ae7b0a525ce831f0463edcf253611f414dc1c20
manhnguyenduc122 <manh.nguyenduc@powergateaustralia.com>
2022-07-27 18:28:25 +0700

    migrate turbo-links to turbo-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery
 //= require bootstrap
 //= require cocoon
-//= require turbolinks
 //= require_tree .
---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,18 @@
+// Menu manipulation
+
+// Add toggle listeners to listen for clicks.
+document.addEventListener("turbo:load", function() {
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
+  });
+
+  let account = document.querySelector("#account");
+  account.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu = document.querySelector("#dropdown-menu")
+    menu.classList.toggle("active");
+  });
+});
