1ec2caeccedae736e1eb6f096dfea07882bee193
sonhle9 <sonhle9@gmail.com>
2022-08-03 06:20:31 +0700

    rails g model message room:references content:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_020814) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_020815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_020814) do
   create_table "guest_wishes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
   end
 
   create_table "microposts", force: :cascade do |t|
@@ -239,6 +247,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_020814) do
   add_foreign_key "guest_wish_items", "guest_wishes"
   add_foreign_key "guest_wish_items", "products"
   add_foreign_key "guest_wish_items", "variants"
+  add_foreign_key "messages", "rooms"
   add_foreign_key "microposts", "users"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
