a1d5f3ec91fdeaef22bdb650338959c08f96e97b
sonhle9 <sonhle9@gmail.com>
2022-08-03 22:53:12 +0700

    https://github.com/hotwired/hotwire-rails-demo-chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :room
+  broadcasts_to :room
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
-  has_many :messages
+  has_many :messages, dependent: :destroy
+  broadcasts
 end
