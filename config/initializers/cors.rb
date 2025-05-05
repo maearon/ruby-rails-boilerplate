74754ef568951a6911967f3949eb10a72f2a55cf
mark <manhng132@gmail.com>
2025-05-05 17:32:55 +0700

    Update cors.rb for https://vue-boilerplate-psi.vercel.app/

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -126,4 +126,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins 'https://vue-boilerplate-psi.vercel.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
