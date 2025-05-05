ab2a04eeaf64ff2881ee37846692c654c9fdd238
mark <manhng132@gmail.com>
2025-05-05 10:56:59 +0700

    Update cors.rb for vue-boilerplate localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -72,6 +72,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins 'http://localhost:5173'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
     origins 'https://ruby-rails-boilerplate-chv2p231v-maearons-projects.vercel.app'
 
     resource '*',
