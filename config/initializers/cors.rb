8d790cdeea20136413c4948d49429ae504cda309
maearon <manhng132@gmail.com>
2025-05-07 21:17:01 +0700

    add cors for expo react native local app run rails with rails s -b 0.0.0.0 -p 3000

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -150,4 +150,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins 'http://192.168.1.7:8081', 'exp://192.168.1.7:8081', 'http://localhost:8081'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
