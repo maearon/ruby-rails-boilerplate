8cfa55c529102137447bd05b4c5553d3e46cf0ad
mark <manhng132@gmail.com>
2025-08-01 14:10:06 +0700

    Update entrypoint.sh
    Signed-off-by: mark <manhng132@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 echo "🔧 Chuẩn bị database..."
 bundle exec bin/rails db:prepare # db:create + db:migrate
 
-echo "🌱 Seed dữ liệu..."
-bundle exec bin/rails db:seed
+# echo "🌱 Seed dữ liệu..."
+# bundle exec bin/rails db:seed
 
 echo "🧹 Xoá server.pid cũ nếu có..."
 rm -f /app/tmp/pids/server.pid
